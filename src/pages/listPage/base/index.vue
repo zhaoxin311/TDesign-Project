da39292b3eba5be9456e990e4d2b9f9ebfd4b030
zhaoxin0311 <1204090769@qq.com>
2022-02-23 17:57:59 +0800

    测试页添加代码

--- src/pages/listPage/base/index.vue
+++ src/pages/listPage/base/index.vue
@@ -1,3 +1,34 @@
 <template>
-  <div>列表测试页</div>
+  <div class="list-common-table">
+    <t-row>
+      <t-col :span="3" >
+        <div  style="background:red">
+          <t-button theme="primary">新建合同</t-button>
+          <t-button theme="default">导出合同</t-button>
+          <span>已选 2 项</span>
+        </div>
+      </t-col>
+      <t-col :span="5"></t-col>
+      <t-col :span="4" >
+        <div style="background:green">
+          <t-input
+            clearable
+            v-model="value"
+            placeholder="请输入您需要搜索的内容"
+            @clear="onClear"
+          >
+          </t-input>
+        </div>
+      </t-col>
+    </t-row>
+  </div>
 </template>
+
+<style scoped lang="less">
+@import '@/style/variables.less';
+.list-common-table{
+  background-color: @bg-color-container;
+  padding: 30px 32px;
+  border-radius: @border-radius;
+}
+</style>
